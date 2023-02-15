806cc0bc86a62e67c504c51c32c68d7a0769f6a8
Edgar <edgarnikogosian89@gmail.com>
2023-02-15 18:36:17 +0400

    Testing markdown parser on keydown event

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -12,6 +12,37 @@ const markdownParser = (text: string) => {
   return toHTML.trim();
 };
 
+class MyParagraph extends Paragraph {
+  constructor({ data, api, config, readOnly, block }: any) {
+    super({ data, api, config, readOnly, block });
+    //@ts-ignore
+    this._element.addEventListener('keydown', (e) => {
+      if (bold.test(e.target.innerHTML) || italics.test(e.target.innerHTML)) {
+        if ('Enter' === e.code) {
+          //@ts-ignore
+          this._element.innerHTML = markdownParser(e.target.innerHTML);
+          //@ts-ignore
+          const p = this._element as any;
+          //@ts-ignore
+          const length = p.lastChild?.innerHTML ? 1 : p.lastChild.length;
+
+          const s = window.getSelection();
+          const r = document.createRange();
+          r.setStart(p.lastChild, length);
+          r.setEnd(p.lastChild, length);
+          s?.removeAllRanges();
+          s?.addRange(r);
+        }
+      }
+    });
+  }
+
+  save(blockContent: any) {
+    const content = Paragraph.prototype.save(blockContent);
+    return { ...content, text: markdownParser(content.text) };
+  }
+}
+
 const initialData = () => {
   return {
     time: new Date().getTime(),
@@ -51,46 +82,48 @@ const Editor = () => {
         ejInstance.current = editor;
       },
 
-      onChange: async (e) => {
-        // console.log(e);
-        // let content = await e.saver.save();
-        let reRender = false;
-        // console.log(e);
-        const index = e.blocks.getCurrentBlockIndex();
-
-        if (index >= 0) {
-          let currentBlock = e.blocks.getBlockByIndex(index);
-          console.log(currentBlock?.config);
-          if (currentBlock) {
-            //   if (currentBlock.data?.text) {
-            //     const text = currentBlock.data?.text;
-            //     reRender = bold.test(currentBlock.data?.text);
-            //     currentBlock.data = { ...currentBlock.data, text: markdownParser(text) };
-            //   }
-            // }
-            // const updatedBlocks = {
-            //   time:new Date().getTime(),
-            //   blocks: content.blocks.map((item, idx) =>
-            //     idx === index ? { ...item, data: { ...currentBlock.data } } : item
-            //   )
-            // };
-            // console.log('-------');
-            // console.log(currentBlock?.id);
-            // console.log(reRender);
-            // console.log('-------');
-            // if (currentBlock?.id && reRender) {
-            //   if (reRender) {
-            //     e.blocks.update(currentBlock.id, { ...currentBlock.data });
-            //   }
-          }
-          // setEditorData({ ...updatedBlocks });
+      onChange: async (api, event) => {
+        if (event.type !== 'block-changed') {
+          return;
         }
+
+        const id: string = event.detail.target.id;
+
+        api.saver.save().then((content) => {
+          let reRender = false;
+
+          if (id) {
+            let currentBlock = content.blocks.find((item) => item.id === id);
+
+            if (currentBlock) {
+              if (currentBlock.data?.text) {
+                const text = currentBlock.data?.text;
+                reRender = bold.test(currentBlock.data?.text);
+                currentBlock.data = { ...currentBlock.data, text: markdownParser(text) };
+              }
+            }
+            const updatedBlocks = {
+              time: new Date().getTime(),
+              blocks: content.blocks.map((item) =>
+                item.id === id ? { ...item, data: { ...currentBlock?.data } } : item
+              )
+            };
+
+            if (currentBlock?.id && reRender) {
+              if (reRender) {
+                api.blocks.update(currentBlock.id, { ...currentBlock.data });
+              }
+            }
+            setEditorData({ ...updatedBlocks });
+          }
+        });
       },
       autofocus: true,
       tools: {
         header: Header,
         paragraph: {
-          class: Paragraph,
+          //@ts-ignore
+          class: MyParagraph,
           inlineToolbar: true,
           config: {
             preserveBlank: true
@@ -100,16 +133,9 @@ const Editor = () => {
     });
   };
 
-  const test = async (e: React.KeyboardEvent<HTMLDivElement>) => {
-    if (ejInstance.current && e.key === 'Enter') {
-      // let content = await ejInstance.current.save();
-      console.log(editorData);
-    }
-  };
-
   return (
     <>
-      <div id="editorjs" onKeyUp={(e) => test(e)} />
+      <div id="editorjs" />
     </>
   );
 };
