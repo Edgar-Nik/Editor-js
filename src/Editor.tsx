b23a17b312a8bacc9b70dc0744073789983f7f71
Edgar <edgarnikogosian89@gmail.com>
2023-02-16 17:35:05 +0400

    removed comments

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -9,48 +9,6 @@ import SimpleImage from '@editorjs/simple-image';
 import { useEffect, useRef, useState } from 'react';
 import DragDrop from 'editorjs-drag-drop';
 
-// const bold = /\*\*(.*)\*\*/gim;
-// const italic = /(?<!\*)\*(?![*\s])(?:([^*]*[^*\s]))?\*/gim;
-
-// let lastOffsetKey = 1;
-// const time = new Date().getTime();
-
-// const markdownParser = (text: string, offsetKey: number, tag: string) => {
-//   let tmp = text;
-//   let helpers = `&#8203;<span data-offset-key="${time}-${offsetKey}"></span>`;
-//   if (tag === 'bold') {
-//     return tmp.replace(bold, `<b>$1</b>${helpers}`);
-//   } else if (tag === 'italic') {
-//     return tmp.replace(italic, `<i>$1</i>${helpers}`);
-//   }
-//   return tmp;
-// };
-
-// class MyParagraph extends Paragraph {
-//   constructor({ data, api, config, readOnly, block }: any) {
-//     super({ data, api, config, readOnly, block });
-//     this._element.addEventListener('keyup', (e: any) => {
-//       if (bold.test(e.target.innerHTML)) {
-//         //@ts-ignore
-//         this._element.innerHTML = markdownParser(e.target.innerHTML, lastOffsetKey, 'bold');
-//       } else if (italic.test(e.target.innerHTML)) {
-//         //@ts-ignore
-//         this._element.innerHTML = markdownParser(e.target.innerHTML, lastOffsetKey, 'italic');
-//       }
-//       const element = document.querySelector(`[data-offset-key="${time}-${lastOffsetKey}"]`);
-//       lastOffsetKey += 1;
-//       if (element) {
-//         const s = window.getSelection();
-//         const r = document.createRange();
-//         r.setStart(element, 0);
-//         r.setEnd(element, 0);
-//         s?.removeAllRanges();
-//         s?.addRange(r);
-//       }
-//     });
-//   }
-// }
-
 const initialData = () => {
   return {
     time: new Date().getTime(),
