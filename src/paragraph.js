a0567bbfdd2fc4c69c9e27312a1008d9c1eb6b13
Edgar <edgarnikogosian89@gmail.com>
2023-02-17 13:34:55 +0400

    Added headings

--- src/paragraph.js
+++ src/paragraph.js
@@ -1,7 +1,13 @@
 import EditorParagraph from '@editorjs/paragraph';
 
 const bold = /\*\*(.*)\*\*/gim;
+const strike = /~(.*)~/gim;
 const italic = /(?<!\*)\*(?![*\s])(?:([^*]*[^*\s]))?\*/gim;
+const code = /```(.*)```/gim;
+const heading3 = /^###&nbsp;(.*)|^### (.*)/gim;
+const heading2 = /^##&nbsp;(.*)|^### (.*)/gim;
+const heading1 = /^#&nbsp;(.*)|^### (.*)/gim;
+const heading = /#+&nbsp;|#+ (.*)/gim;
 
 let lastOffsetKey = 1;
 const time = new Date().getTime();
@@ -14,6 +20,10 @@ const markdownParser = (text, offsetKey, tag) => {
     return tmp.replace(bold, `<b>$1</b>${helpers}`);
   } else if (tag === 'italic') {
     return tmp.replace(italic, `<i>$1</i>${helpers}`);
+  } else if (tag === 'strike') {
+    return tmp.replace(strike, `<strike>$1</strike>${helpers}`);
+  } else if (tag === 'code') {
+    return tmp.replace(code, `<code>$1</code>${helpers}`);
   }
 
   return tmp;
@@ -23,12 +33,23 @@ class Paragraph extends EditorParagraph {
     super({ data, api, config, readOnly, block });
 
     this._element.addEventListener('keyup', (e) => {
-      if (bold.test(e.target.innerHTML)) {
+      if (e.code === 'Space') {
+        if (heading3.test(e.target.innerHTML)) {
+          this.insertHeading(3, e.target.innerHTML);
+        } else if (heading2.test(e.target.innerHTML)) {
+          this.insertHeading(2, e.target.innerHTML);
+        } else if (heading1.test(e.target.innerHTML)) {
+          this.insertHeading(1, e.target.innerHTML);
+        }
+      } else if (bold.test(e.target.innerHTML)) {
         this._element.innerHTML = markdownParser(e.target.innerHTML, lastOffsetKey, 'bold');
       } else if (italic.test(e.target.innerHTML)) {
         this._element.innerHTML = markdownParser(e.target.innerHTML, lastOffsetKey, 'italic');
+      } else if (strike.test(e.target.innerHTML)) {
+        this._element.innerHTML = markdownParser(e.target.innerHTML, lastOffsetKey, 'strike');
+      } else if (code.test(e.target.innerHTML)) {
+        this._element.innerHTML = markdownParser(e.target.innerHTML, lastOffsetKey, 'code');
       }
-
       const element = document.querySelector(`[data-offset-key="${time}-${lastOffsetKey}"]`);
       lastOffsetKey += 1;
 
@@ -41,6 +62,24 @@ class Paragraph extends EditorParagraph {
         selection?.addRange(range);
       }
     });
+  }
+
+  insertHeading(level, text) {
+    const index = this.api.blocks.getCurrentBlockIndex();
+
+    this.api.blocks.delete(index);
+
+    this.api.blocks.insert(
+      'header',
+      {
+        text: text.replace(heading, '$1'),
+        level
+      },
+      undefined,
+      index
+    );
+
+    this.api.caret.setToBlock(index);
   }
 }
 
